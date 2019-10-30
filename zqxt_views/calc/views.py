8b21aff6ac35db18c75c21f329d60f2d9330628a
az198071123 <az198071123@gmail.com>
2019-10-30 23:38:58 +0800

    Initial commit

---
+++ zqxt_views/calc/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def add(request):
+    a = request.GET['a']
+    b = request.GET['b']
+    c = int(a)+int(b)
+    return HttpResponse(str(c))
+
+def add2(request, a, b):
+    c = int(a) + int(b)
+    return HttpResponse(str(c))
+
+def index(request):
+    return render(request, 'home.html')
